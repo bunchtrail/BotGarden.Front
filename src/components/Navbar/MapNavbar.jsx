a516246088d2a301fa1ab55d931e0692b1762e0d
Nikita Lolenko <loleenko@vk.com>
2024-07-06 14:12:05 +0300

    map's nav

--- src/components/Navbar/MapNavbar.jsx
+++ src/components/Navbar/MapNavbar.jsx
@@ -2,22 +2,38 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Button from '../Buttons/Button/ButtonHref';
 
-function MapNavbar() {
+function MapNavbar({ setMapMode }) {
   return (
     <div id="navbar" className="rounded-navbar fixed-top navsect">
       <Link to="/" className="nav-item">
         <i className="fas fa-home" /> Вернуться на главный экран
       </Link>
-      <Button iconClass="fas fa-plus-circle" className="btn-primary">
+      <Button
+        iconClass="fas fa-plus-circle"
+        className="btn-primary"
+        onClick={() => setMapMode('addArea')}
+      >
         Добавить область
       </Button>
-      <Button iconClass="fas fa-edit" className="btn-warning">
+      <Button
+        iconClass="fas fa-edit"
+        className="btn-warning"
+        onClick={() => setMapMode('editArea')}
+      >
         Изменить область
       </Button>
-      <Button iconClass="fas fa-trash-alt" className="btn-danger">
+      <Button
+        iconClass="fas fa-trash-alt"
+        className="btn-danger"
+        onClick={() => setMapMode('deleteArea')}
+      >
         Удалить область
       </Button>
-      <Button iconClass="fas fa-seedling" className="btn-danger">
+      <Button
+        iconClass="fas fa-seedling"
+        className="btn-danger"
+        onClick={() => setMapMode('deletePlants')}
+      >
         Удалить растения в области
       </Button>
     </div>
--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -5,6 +5,7 @@ import MapComponent from '../assets/js/Map/mapComponent';
 function MapPage() {
   const [latitude, setLatitude] = useState(51.505);
   const [longitude, setLongitude] = useState(-0.09);
+  const [mapMode, setMapMode] = useState('view'); // Добавляем состояние для режима карты
 
   const customMapStyle = {
     height: '650px', // Увеличение высоты карты
@@ -15,17 +16,17 @@ function MapPage() {
 
   return (
     <>
-      <MapNavbar />
+      <MapNavbar setMapMode={setMapMode} />
       <MapComponent
         latitude={latitude}
         longitude={longitude}
         setLatitude={setLatitude}
         setLongitude={setLongitude}
-        allowMarker
-        allowArea
-        allowEdit
-        allowDelete
-        mapStyle={customMapStyle} // Передача кастомных стилей
+        allowMarker={mapMode === 'marker'}
+        allowArea={mapMode === 'addArea'}
+        allowEdit={mapMode === 'editArea'}
+        allowDelete={mapMode === 'deleteArea' || mapMode === 'deletePlants'}
+        mapStyle={customMapStyle}
       />
     </>
   );
